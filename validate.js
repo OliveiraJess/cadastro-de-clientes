413f51d3bdd52336d9923b48fa8b002349505237
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-11-03 10:50:21 -0300

    mask: mascara celular

--- validate.js
+++ validate.js
@@ -46,3 +46,16 @@ function validate(event) {
 
   return valid;
 }
+
+const handleCell = (event) => {
+  let input = event.target
+  input.value = cellMask(input.value)
+}
+
+const cellMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g,'')
+  value = value.replace(/(\d{2})(\d)/,"($1) $2")
+  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+  return value
+}
