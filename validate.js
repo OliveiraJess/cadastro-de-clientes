764f353d5c6b9964dd950c0b45011fac223503b4
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-11-03 09:29:17 -0300

    validate: validação email

--- validate.js
+++ validate.js
@@ -6,6 +6,8 @@ function validate(event) {
   event.preventDefault();
 
   const name = document.getElementById("nome");
+  const email = document.getElementById("email");
+
   let valid = true;
 
   if (!name.value) {
@@ -15,5 +17,14 @@ function validate(event) {
     nameError.setAttribute("aria-hidden", false);
     nameError.setAttribute("aria-invalid", true);
   }
+
+  if (!email.value) {
+    const nameError = document.getElementById("emailError");
+    emailError.classList.add("visible");
+    email.classList.add("invalid");
+    emailError.setAttribute("aria-hidden", false);
+    emailError.setAttribute("aria-invalid", true);
+  }
+
   return valid;
 }
